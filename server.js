215d52de430ab00a133e0ecaccd2de87be361ef2
Santel916 <relsdssdsds@gmail.com>
2025-04-14 16:18:35 +0300

    Add files via upload
    Signed-off-by: Santel916 <relsdssdsds@gmail.com>

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+const stripe = require('stripe')('sk_test_51RCN4OPRHC7jcWnU8ZIIwbZkOQFFOrOIZm6p5cvC1huidHtcVYK3i6C7HoEyyqSrSJONjxECWGCiy2Yf2xCXvLhS00sBMCEiMe'); // Replace with your real secret key
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.static('public'));
+app.use(express.json());
+
+app.post('/create-checkout-session', async (req, res) => {
+  const { amount } = req.body;
+
+  try {
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ['card'],
+      line_items: [
+        {
+          price_data: {
+            currency: 'eur',
+            product_data: {
+              name: 'In-Game Currency',
+            },
+            unit_amount: Math.round(amount * 100), // Stripe uses cents
+          },
+          quantity: 1,
+        },
+      ],
+      mode: 'payment',
+      success_url: 'https://growmagplants.com/succes.html',
+      cancel_url: 'https://growmagplants.com/cancel.html',
+    });
+
+    res.json({ id: session.id });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.listen(5000, () => console.log('Server running on http://localhost:5000'));
